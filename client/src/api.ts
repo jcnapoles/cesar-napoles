17e4c9d4ac056b44c0bf65e72dcd453ea425f4bc
Julio Cesar Napoles Puente <juliocesarnapolespuente@gmail.com>
2023-07-23 13:59:54 +0200

    fixhot-0.0.1

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Asegúrate de que el puerto coincida con el del servidor
+const API_URL = 'http://backend-service/api'; // Asegúrate de que el puerto coincida con el del servidor
 
 export const getHelloMessage = async () => {
   let response = await axios.get(`${API_URL}/hello`);
